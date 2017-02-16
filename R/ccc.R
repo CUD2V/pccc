2779b816bec6340dbd18ff3c418e3ec44ad76fa0
Peter DeWitt (latitude) <peter.dewitt@ucdenver.edu>
2017-02-15 21:47:53 -0700

    added placeholder file for the ccc function

---
+++ R/ccc.R
@@ -0,0 +1,20 @@
+#' Complex Chronic Conditions (CCC)
+#'
+#' Generate CCC and CCC subcategory flags and the number of categories.
+#'
+#' The \code{data.frame} passed to the function should be in wide format.
+#'
+#' @references
+#' Feudtner C, et al. Pediatric complex chronic conditions classification system version 2:
+#'
+#' @param .data a \code{data.frame} containing a patient id and all the ICD-9-CM
+#' or ICD-10-CM codes.
+#' @param id bare name of the column containing the patient id
+#' @param dx_cols using the select functions from dplyr.
+#' @param px_cols the column(s) containing the procedure codes.
+#' @param icdv ICD version 9 or 10, defaults to 10
+#'
+#' @export
+ccc <- function(.data, id, dx_cols, px_cols, icdv = 10) {
+}
+
