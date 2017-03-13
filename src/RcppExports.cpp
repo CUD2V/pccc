a0db02c7f3c01dc41f74daeade29b6a3b4e11ccd
Peter DeWitt (optiplex) <peter.dewitt@ucdenver.edu>
2017-03-13 16:59:34 -0600

    code class built.  building ccc function in c++

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,47 +5,27 @@
 
 using namespace Rcpp;
 
-// code_test
-Rcpp::List code_test(int version);
-RcppExport SEXP pccc_code_test(SEXP versionSEXP) {
+// get_codes
+Rcpp::List get_codes(int version);
+RcppExport SEXP pccc_get_codes(SEXP versionSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
     Rcpp::traits::input_parameter< int >::type version(versionSEXP);
-    rcpp_result_gen = Rcpp::wrap(code_test(version));
+    rcpp_result_gen = Rcpp::wrap(get_codes(version));
     return rcpp_result_gen;
 END_RCPP
 }
-// dx_codes
-Rcpp::List dx_codes(int version);
-RcppExport SEXP pccc_dx_codes(SEXP versionSEXP) {
+// ccc_rcpp
+Rcpp::NumericVector ccc_rcpp(std::vector<std::string> dx, std::vector<std::string> pc, int version);
+RcppExport SEXP pccc_ccc_rcpp(SEXP dxSEXP, SEXP pcSEXP, SEXP versionSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< std::vector<std::string> >::type dx(dxSEXP);
+    Rcpp::traits::input_parameter< std::vector<std::string> >::type pc(pcSEXP);
     Rcpp::traits::input_parameter< int >::type version(versionSEXP);
-    rcpp_result_gen = Rcpp::wrap(dx_codes(version));
-    return rcpp_result_gen;
-END_RCPP
-}
-// pc_codes
-Rcpp::List pc_codes(int version);
-RcppExport SEXP pccc_pc_codes(SEXP versionSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< int >::type version(versionSEXP);
-    rcpp_result_gen = Rcpp::wrap(pc_codes(version));
-    return rcpp_result_gen;
-END_RCPP
-}
-// dx_nuromusc
-Rcpp::StringVector dx_nuromusc(int version);
-RcppExport SEXP pccc_dx_nuromusc(SEXP versionSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< int >::type version(versionSEXP);
-    rcpp_result_gen = Rcpp::wrap(dx_nuromusc(version));
+    rcpp_result_gen = Rcpp::wrap(ccc_rcpp(dx, pc, version));
     return rcpp_result_gen;
 END_RCPP
 }
---
+++ src/pccc.h
@@ -0,0 +1,67 @@
+#include <string>
+#include <Rcpp.h>
+
+#ifndef PCCC_H
+#define PCCC_H
+
+class codes {
+  private:
+    int version;
+
+    std::vector<std::string> dx_neuromusc; 
+    std::vector<std::string> dx_cvd; 
+    std::vector<std::string> dx_respiratory;
+    std::vector<std::string> dx_renal;
+    std::vector<std::string> dx_gi;
+    std::vector<std::string> dx_hemato_immu;
+    std::vector<std::string> dx_metabolic;
+    std::vector<std::string> dx_congeni_genetic;
+    std::vector<std::string> dx_malignancy;
+    std::vector<std::string> dx_neonatal;
+    std::vector<std::string> dx_tech_dep;
+    std::vector<std::string> dx_transplant;
+
+    std::vector<std::string> pc_neuromusc;
+    std::vector<std::string> pc_cvd;
+    std::vector<std::string> pc_respiratory;
+    std::vector<std::string> pc_renal;
+    std::vector<std::string> pc_gi;
+    std::vector<std::string> pc_hemato_immu;
+    std::vector<std::string> pc_metabolic;
+    //std::vector<std::string> pc_congeni_genetic;
+    std::vector<std::string> pc_malignancy;
+    //std::vector<std::string> pc_neonatal;
+    std::vector<std::string> pc_tech_dep;
+    std::vector<std::string> pc_transplant;
+
+  public:
+    codes(int v = 9);
+
+    int get_version() { return version;};
+
+    std::vector<std::string> get_dx_neuromusc()       { return dx_neuromusc; };
+    std::vector<std::string> get_dx_cvd()             { return dx_cvd; };
+    std::vector<std::string> get_dx_respiratory()     { return dx_respiratory; };
+    std::vector<std::string> get_dx_renal()           { return dx_renal; };
+    std::vector<std::string> get_dx_gi()              { return dx_gi; };
+    std::vector<std::string> get_dx_hemato_immu()     { return dx_hemato_immu; };
+    std::vector<std::string> get_dx_metabolic()       { return dx_metabolic; };
+    std::vector<std::string> get_dx_congeni_genetic() { return dx_congeni_genetic; };
+    std::vector<std::string> get_dx_malignancy()      { return dx_malignancy; };
+    std::vector<std::string> get_dx_neonatal()        { return dx_neonatal; };
+    std::vector<std::string> get_dx_tech_dep()        { return dx_tech_dep; };
+    std::vector<std::string> get_dx_transplant()      { return dx_transplant; };
+                                                                           
+    std::vector<std::string> get_pc_neuromusc()       { return pc_neuromusc; };
+    std::vector<std::string> get_pc_cvd()             { return pc_cvd; };
+    std::vector<std::string> get_pc_respiratory()     { return pc_respiratory; };
+    std::vector<std::string> get_pc_renal()           { return pc_renal; };
+    std::vector<std::string> get_pc_gi()              { return pc_gi; };
+    std::vector<std::string> get_pc_hemato_immu()     { return pc_hemato_immu; };
+    std::vector<std::string> get_pc_metabolic()       { return pc_metabolic; };
+    std::vector<std::string> get_pc_malignancy()      { return pc_malignancy; };
+    std::vector<std::string> get_pc_tech_dep()        { return pc_tech_dep; };
+    std::vector<std::string> get_pc_transplant()      { return pc_transplant; };
+};
+
+#endif
