5538c435a9462c26c3ecfff95aaf0f817db4b78b
Seth Russell <magic-lantern@users.noreply.github.com>
2017-10-18 17:21:29 -0600

    Fixing bug discovered when building out unit tests. If only one patient is passed in and the patient only has one diagnosis or procedure, the ccc_mat_rcpp call fails

--- R/ccc.R
+++ R/ccc.R
@@ -53,12 +53,18 @@ ccc.data.frame <- function(data, id, dx_cols, pc_cols, icdv) {
 
   if (!missing(dx_cols)) {
     dxmat <- sapply(dplyr::select(data, !!dplyr::enquo(dx_cols)), as.character)
+    if(! is.matrix(dxmat)) {
+      dxmat <- as.matrix(dxmat)
+    }
   } else {
     dxmat <- matrix("", nrow = nrow(data))
   }
 
   if (!missing(pc_cols)) {
     pcmat <- sapply(dplyr::select(data, !!dplyr::enquo(pc_cols)), as.character)
+    if(! is.matrix(pcmat)) {
+      pcmat <- as.matrix(pcmat)
+    }
   } else {
     pcmat <- matrix("", nrow = nrow(data))
   }
