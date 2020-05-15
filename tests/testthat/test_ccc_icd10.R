d45f0d025aecb431a0c45b96e4d1d6cf1cc9919e
Seth Russell <magic-lantern@users.noreply.github.com>
2020-05-14 20:42:15 -0600

    Discovered that a dataframe column generated with letters[x:y] is a factor in 3.6.3 but in R 4.x is a character column. Modified test to convert to the same column type.

--- tests/testthat/test_ccc_icd10.R
+++ tests/testthat/test_ccc_icd10.R
@@ -16,18 +16,19 @@ context("PCCC - ccc ICD10 function tests")
 
 test_that("random data set with all parameters ICD10 - result should be unchanged.", {
   # saved as random_data_test_result
+  ccc_out <- ccc(data.frame(id = letters[1:3],
+                            dx1 = c('sadcj89sa', '1,2.3.4,5', 'sdf 9'),
+                            pc1 = c('da89v#$%', ' 90v_', 'this is a super long string compared to standard ICD codes and shouldnt break anything - if it does, the world will come to an end... Ok, so maybe not, but that means I need to fix something in this package.'),
+                            other_col = LETTERS[1:3]),
+                 id      = id,
+                 dx_cols = dplyr::starts_with("dx"),
+                 pc_cols = dplyr::starts_with("pc"),
+                 icdv    = 10)
+  ccc_out$id <- as.factor(ccc_out$id)
+  rnd_test <- test_helper(random_data_test_result)
+  rnd_test$id <- as.factor(rnd_test$id)
   expect_true(
-    all.equal(
-      ccc(data.frame(id = letters[1:3],
-                     dx1 = c('sadcj89sa', '1,2.3.4,5', 'sdf 9'),
-                     pc1 = c('da89v#$%', ' 90v_', 'this is a super long string compared to standard ICD codes and shouldnt break anything - if it does, the world will come to an end... Ok, so maybe not, but that means I need to fix something in this package.'),
-                     other_col = LETTERS[1:3]),
-          id      = id,
-          dx_cols = dplyr::starts_with("dx"),
-          pc_cols = dplyr::starts_with("pc"),
-          icdv    = 10),
-      test_helper(random_data_test_result)
-    )
+    all.equal(ccc_out, rnd_test)
   )
 })
 
