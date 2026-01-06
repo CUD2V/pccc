c8b62124c8c2a31b6b84b1f94a56d470cad5b35c
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2026-01-06 10:31:04 -0700

    Add ICD-9 V42.0 to transplant set
    Prior to this change V42.0 only mapped to renal when it needed to map to
    renal and transplant.
    
    This change add V420 to the ICD-9 dx_transplant set
    
    fix CUD2V/pccc#54

--- tests/test_get_codes.R
+++ tests/test_get_codes.R
@@ -44,7 +44,7 @@ expected_counts <- read.table(sep = "|", header = TRUE, strip.white = TRUE,
            renal |        dx |     9 |   18
      respiratory |        dx |     9 |   17
         tech_dep |        dx |     9 |   49
-      transplant |        dx |     9 |   19
+      transplant |        dx |     9 |   20
              cvd |  dx_fixed |     9 |    2
        neuromusc |  dx_fixed |     9 |    2
      respiratory |  dx_fixed |     9 |    2
